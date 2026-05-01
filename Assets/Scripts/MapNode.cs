a567b8eaa0f34c3a994dc9cad60380c1083b7f8c
CooperBlatz <cblatz@umassd.edu>
2026-04-30 20:00:28 -0400

    Map 2.1?

--- Assets/Scripts/MapNode.cs
+++ Assets/Scripts/MapNode.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MapNode : MonoBehaviour
+public class MapNode : MonoBehaviour, IPointerClickHandler
 {
     public MapNode[] connectedNodes;
     public bool isUnlocked = false;
 
     private MapManager mapManager;
+    private Image image;
+
+    public enum NodeState
+    {
+        Current,
+        Visited,
+        Skipped,
+        Available,
+        Locked
+    }
+
+    public NodeState currentState;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+
+        if (image == null)
+        {
+            Debug.LogError(gameObject.name + " missing Image component"); 
+        }
+    }
 
     public void SetManager(MapManager manager)
     {
@@ -17,15 +41,42 @@ public class MapNode : MonoBehaviour
         isUnlocked = true;
     }
 
-    public void ClickNode()
+    public void SetState(NodeState state)
     {
-        if (!isUnlocked) return;
+        currentState = state;
 
-        Debug.Log("Clicked: " + gameObject.name);
+        if (image == null) return;
 
-        if (mapManager != null)
+        switch (state)
         {
-            mapManager.MoveToNode(this);
+            case NodeState.Current:
+                image.color = new Color(0.3f, 0.8f, 0.3f);
+                break;
+
+            case NodeState.Visited:
+                image.color = new Color(0.6f, 1f, 0.6f);
+                break;
+
+            case NodeState.Skipped:
+                image.color = new Color(0.75f, 0.75f, 0.75f);
+                break;
+
+            case NodeState.Available:
+                image.color = new Color(1f, 1f, 0.6f);
+                break;
+
+            case NodeState.Locked:
+                image.color = new Color(0.75f, 0.75f, 0.75f);
+                break;
         }
+    }
+
+   //click the node
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isUnlocked || currentState != NodeState.Available)
+            return;
+
+        mapManager?.MoveToNode(this);
     }
 }
